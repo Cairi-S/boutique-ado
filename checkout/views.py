8cc79188f34dbef6d9bcc2a6d1847166355b044a
Cairi-S <cairi.swainson@gmail.com>
2021-09-13 14:17:22 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JZELMJPt6gs7S5Rcr7bcve8cFw8NA7oZGGTYmP6futAdvxnHJkHxEnj1a66j50mwqWkPYjwc4B4XO1gKr03Erzz00kCQRT43a',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
